a3610b4ba72cddd7a67ffd6a2c644b53019a12c8
Marcelo Ramos <marcelor@gmail.com>
2012-12-30 07:27:52 -0200

    Added restful api.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.restless import APIManager
 from local_settings import SQLALCHEMY_DATABASE_URI, DEBUG_MODE
 
 app = Flask(__name__)
@@ -23,14 +24,16 @@ class ExchangeRate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     base_currency_id = db.Column("Base currency", db.Integer, db.ForeignKey('currency.id'))
     currency_id = db.Column("Currency", db.Integer, db.ForeignKey('currency.id'))
-    base_currency = db.relationship("Currency", primaryjoin="ExchangeRate.base_currency_id==Currency.id")
-    currency = db.relationship("Currency", primaryjoin="ExchangeRate.currency_id==Currency.id")
-    buy_rate = db.Column("Buy rate", db.Numeric, default=Decimal('0'))
-    sell_rate = db.Column("Sell rate", db.Numeric, default=Decimal('0'))
+    base_currency = db.relationship("Currency", backref='base currencies', primaryjoin="ExchangeRate.base_currency_id==Currency.id")
+    currency = db.relationship("Currency", backref='currencies', primaryjoin="ExchangeRate.currency_id==Currency.id")
+    buy_rate = db.Column("Buy rate", db.Float, default=Decimal('0'))
+    sell_rate = db.Column("Sell rate", db.Float, default=Decimal('0'))
 
     def __repr__(self):
         return '<Exchange rate: %s ---> %s>' % (self.base_currency.iso_code, self.currency.iso_code)
 
+# Create the database tables.
+db.create_all()
 
 @app.route("/")
 def index():
@@ -38,6 +41,17 @@ def index():
     currency = Currency.query.filter_by(iso_code='USD').first()
     rate = ExchangeRate.query.filter_by(base_currency=base_currency, currency=currency).first()
     return render_template('home.html', buy_rate=rate.buy_rate, sell_rate=rate.sell_rate)
+
+
+# Create the Flask-Restless API manager.
+manager = APIManager(app, flask_sqlalchemy_db=db)
+# Create API endpoints, which will be available at /api/<tablename> by
+# default. Allowed HTTP methods can be specified as well.
+manager.create_api(ExchangeRate, methods=['GET'], include_columns=['base_currency', 'base_currency.name', 'base_currency.iso_code', \
+    'currency', 'currency.name', 'currency.iso_code', \
+    'buy_rate', 'sell_rate'], \
+    collection_name='rates')
+
 
 if __name__ == '__main__':
     app.debug = DEBUG_MODE

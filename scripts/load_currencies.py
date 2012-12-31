a71dca2be27f6c61243d256ece45b3483f5d255a
Marcelo Ramos <marcelor@gmail.com>
2012-12-31 04:05:14 -0200

    Moved script to scripts folder.

--- scripts/load_currencies.py
+++ scripts/load_currencies.py
@@ -2,7 +2,7 @@ from main import Currency, db
 from money import CURRENCY
 
 for code, obj in CURRENCY.items():
-    currency = Currency(obj.name, obj.code)
+    currency = Currency(name=obj.name, iso_code=obj.code)
     db.session.add(currency)
 
 db.session.commit()
